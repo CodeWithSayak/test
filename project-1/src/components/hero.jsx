c33f8e7d703a8c3c5f3731a824711d2f0d74bf81
CodeWithSayak <sayakdey2809@gmail.com>
2024-10-28 02:29:45 +0530

    react

--- project-1/src/components/hero.jsx
+++ project-1/src/components/hero.jsx
@@ -1,40 +1,31 @@
-
 const Hero = () => {
+  return (
+    <main className="hero container">
+    <div className="hero-content">
+        <h1>YOUR FEET DESERVE THE BEST</h1>
+        <p>
+          YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP WITH OUR SHOES. YOUR
+          FEET DESERVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR SHOES.
+        </p>
 
-    return <main className="container">
-        <div className="hero">
-            <h1>YOUR FEET DESERVE THE BEST</h1>
-            <p>
-                YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP WITH OUR 
-                SHOES. YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR
-                SHOES.
-            </p>
+        <div className="hero-btn">
+          <button>Shop Now</button>
+          <button className="secondary-btn">Category</button>
+        </div>
 
-            <div className="hero-btn">
-                <button>Shop Now</button>
-                <button>Category</button>
-            </div>
-
-            <div className="shopping">
-                <p>
-                   also available on 
-                </p>
-
-
+        <div className="shopping">
+            <p>also available on</p>
                 <div className="brand-images">
-
-                    <img src="/images/amazon.png" alt="amazon-logo"/>
-                    <img src="/images/flipkart.png" alt="flipkart-logo"/>
-
+                    <img src="/images/amazon.png" alt="amazon-logo" />
+                    <img src="/images/flipkart.png" alt="flipkart-logo" />
                 </div>
-            </div>
         </div>
-        <div className="hero-image">
-            <img src="/images/shoe_image.png" alt="hero-image"/>
+    </div>
+    <div className="hero-image">
+          <img src="/images/shoe_image.png" alt="hero-image" />
         </div>
-
     </main>
-
-}
+  );
+};
 
 export default Hero;
